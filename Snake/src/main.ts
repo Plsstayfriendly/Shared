222f6dec47ee061ee99f29d3a2680ad39da3c52a
Plsstayfriendly <Bentik@gmx.de>
2022-11-29 17:16:53 +0100

    images; all classes in main

--- Snake/src/main.ts
+++ Snake/src/main.ts
@@ -1,4 +1,3 @@
-//import p5 from "p5";
 import "./style.css";
 
 import "../node_modules/p5/lib/p5.js";
@@ -7,75 +6,11 @@ window.p5 = p5;
 
 import "../node_modules/p5/lib/addons/p5.sound.js";
 
-import { Coin } from "./coins"
-import { coins } from "./coins"
-import { Health } from "./health"
-import { healthpack } from "./health"
-
-class Obstacle {
-  constructor(private x_min: number, private x_max: number, private y_min: number, private y_max: number, private w: number, private h: number, private obstacle_speed: number, private x: number, private y: number) {
-  }
-
-  randomizer() {
-    function getRndInteger(min: any, max: any) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-    this.x = getRndInteger(this.x_min, this.x_max)//Horizontaler Spawnbereich
-    this.y = getRndInteger(this.y_min, this.y_max) //Vertikaler Spawnbereich
-  }
-
-  execute_in_draw_1(p: p5) { //Wird in der draw function aufgegriffen und ausgeführt
-    p.fill(200, 50, 30)
-    p.rect(this.x, this.y, this.w, this.h) //Werte werden in der draw function eingesetzt
-  }
-
-  execute_in_draw_2() {
-    this.x = this.x - this.obstacle_speed
-  }
-
-  collide(player_x: number, player_y: number, player_w: number, player_h: number) {
-
-    if (player_x + player_w + this.w >= this.x + this.w && player_x <= this.x) {
-      if (player_y + player_h + this.h >= this.y + this.h && player_y <= this.y) {
-
-        return true
-      }
-    }
-    return false
-  }
-
-  border() {
-    function getRndInteger(min: any, max: any) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-    if (this.x < 0) {
-      this.x = getRndInteger(this.x_min, this.x_max)
-      this.y = getRndInteger(this.y_min, this.y_max)
-    }
-  }
-}
-
 const instance = new p5((p) => {
 
-  //Player
-  let Player_Width = 90
-  let Player_height = 90
-  let Player_Position_Horizontal = p.windowWidth / 2 - Player_Width / 2
-  let Player_Position_Vertical = p.windowHeight / 2 - Player_height / 2
-  let lives = 3
-  let speed = 6
-  let up = false
-
-  //Deathscreen
-  let space = false
-
-  //Score
-  let coin_item = 0
-
-  //Variablen Klassen
-  let obstacles: Obstacle[] = []
-
+  //Needs to be defined for Preload and Reset
   let maintheme;
+  let Playerimg;
   let loopStart = 0
   let loopDuration = 3
   let soundvalue = 0
@@ -85,18 +20,170 @@ const instance = new p5((p) => {
   p.preload = () => {
     maintheme = p.loadSound("src/maintheme.mp3", () => {
       maintheme.loop()
+      Playerimg = p.loadImage("src/pacimgfinal.png")
     });
+
+    //Load obstacleimg
+    let obstacleimg;
+    obstacleimg = p.loadImage("src/ghostimgfinal.png")
+    class Obstacle {
+      constructor(private x_min: number, private x_max: number, private y_min: number, private y_max: number, private w: number, private h: number, private obstacle_speed: number, private x: number, private y: number) {
+      }
+
+      randomizer() {
+        function getRndInteger(min: any, max: any) {
+          return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+        this.x = getRndInteger(this.x_min, this.x_max)//Horizontaler Spawnbereich
+        this.y = getRndInteger(this.y_min, this.y_max) //Vertikaler Spawnbereich
+      }
+
+      execute_in_draw_1(p: p5) { //Wird in der draw function aufgegriffen und ausgeführt
+        p.image(obstacleimg, this.x, this.y, this.w, this.h) //Werte werden in der draw function eingesetzt
+      }
+
+      execute_in_draw_2() {
+        this.x = this.x - this.obstacle_speed
+      }
+
+      collide(player_x: number, player_y: number, player_w: number, player_h: number) {
+
+        if (player_x + player_w + this.w >= this.x + this.w && player_x <= this.x) {
+          if (player_y + player_h + this.h >= this.y + this.h && player_y <= this.y) {
+
+            return true
+          }
+        }
+        return false
+      }
+
+      border() {
+        function getRndInteger(min: any, max: any) {
+          return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+        if (this.x < 0) {
+          this.x = getRndInteger(this.x_min, this.x_max)
+          this.y = getRndInteger(this.y_min, this.y_max)
+        }
+      }
+    }
+    let coinimg;
+    coinimg = p.loadImage("src/coinimgfinal.png")
+    class Coin {
+      constructor(private x_min: number, private x_max: number, private y_min: number, private y_max: number, private w: number, private h: number, private coin_speed: number, private x: number, private y: number) {
+      }
+
+      coin_randomizer() {
+        function getRndInteger(min: any, max: any) {
+          return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+        this.x = getRndInteger(this.x_min, this.x_max)
+        this.y = getRndInteger(this.y_min, this.y_max)
+      }
+
+      coin_execute_in_draw_1(p: p5) {
+        p.image(coinimg, this.x, this.y, this.w, this.h)
+      }
+
+      coin_execute_in_draw_2() {
+        this.x = this.x - this.coin_speed
+      }
+
+      coin_collect(player_x: number, player_y: number, player_w: number, player_h: number) {
+
+        if (player_x + player_w + this.w >= this.x + this.w && player_x <= this.x) {
+          if (player_y + player_h + this.h >= this.y + this.h && player_y <= this.y) {
+
+            return true
+          }
+        }
+        return false
+      }
+
+      coin_border() {
+        function getRndInteger(min: any, max: any) {
+          return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+        if (this.x < 0) {
+          this.x = getRndInteger(this.x_min, this.x_max)
+          this.y = getRndInteger(this.y_min, this.y_max)
+        }
+      }
+    }
+    let healthimg;
+    healthimg = p.loadImage("src/healthimgfinal.png")
+    class Health {
+      constructor(private x_min: number, private x_max: number, private y_min: number, private y_max: number, private w: number, private h: number, private health_speed: number, private x: number, private y: number) {
+      }
+
+      health_randomizer() {
+        function getRndInteger(min: any, max: any) {
+          return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+        this.x = getRndInteger(this.x_min, this.x_max)
+        this.y = getRndInteger(this.y_min, this.y_max)
+      }
+
+      health_execute_in_draw_1(p: p5) {
+        p.image(healthimg, this.x, this.y, this.w, this.h)
+      }
+
+      health_execute_in_draw_2() {
+        this.x = this.x - this.health_speed
+      }
+
+      health_collect(player_x: number, player_y: number, player_w: number, player_h: number) {
+
+        if (player_x + player_w + this.w >= this.x + this.w && player_x <= this.x) {
+          if (player_y + player_h + this.h >= this.y + this.h && player_y <= this.y) {
+
+            return true
+          }
+        }
+        return false
+      }
+
+      health_border() {
+        function getRndInteger(min: any, max: any) {
+          return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+        if (this.x < 0) {
+          this.x = getRndInteger(this.x_min, this.x_max)
+          this.y = getRndInteger(this.y_min, this.y_max)
+        }
+      }
+    }
+
+    //Player
+    let Player_Width = 90
+    let Player_height = 90
+    let Player_Position_Horizontal = p.windowWidth / 2 - Player_Width / 2
+    let Player_Position_Vertical = p.windowHeight / 2 - Player_height / 2
+    let lives = 3
+    let speed = 6
+    let up = false
+
+    //Deathscreen
+    let space = false
+
+    //Score
+    let coin_item = 0
+
+    //Variablen Klassen
+    let obstacles: Obstacle[] = []
+    let coins: Coin[] = []
+    let healthpack: Health[] = []
 
     //Background  
     p.setup = function setup() {
       p.frameRate(60);
       p.createCanvas(p.windowWidth, p.windowHeight);
-      reset();
+      p.reset();
     }
 
-    function reset(){
+    p.reset = function reset() {
 
-      //reset all variables
+      //reset all variables, except highscore
       Player_Position_Horizontal = p.windowWidth / 2 - Player_Width / 2;
       Player_Position_Vertical = p.windowHeight / 2 - Player_height / 2;
       lives = 3;
@@ -135,7 +222,6 @@ const instance = new p5((p) => {
       healthpack.push(new Health(p.windowWidth + 50, p.windowWidth + 200, p.windowHeight - 100, 0, 35, 35, p.random(5, 11), 0, 0))
     }
 
-
     //Keys
     p.keyPressed = function () {
       if (p.keyCode === p.UP_ARROW)
@@ -156,7 +242,6 @@ const instance = new p5((p) => {
         space = false
       }
     }
-
 
     //Draw
     p.draw = function draw() {
@@ -220,8 +305,7 @@ const instance = new p5((p) => {
       }
 
       //Player
-      p.fill(160, 32, 240)
-      p.rect(Player_Position_Horizontal, Player_Position_Vertical, Player_Width, Player_height)
+      p.image(Playerimg, Player_Position_Horizontal, Player_Position_Vertical, Player_Width, Player_height)
       if (up == true) {
         Player_Position_Vertical = Player_Position_Vertical - speed
       }
@@ -256,9 +340,7 @@ const instance = new p5((p) => {
         p.text("GAME OVER", p.windowWidth / 2, p.windowHeight / 2)
         speed = 0
         if (space == true) {
-          
-          reset();
-
+          p.reset();
         }
       }
     }
